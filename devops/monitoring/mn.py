1ac97151f38a7f8b18dca27e8c7db3d84c49b39f
Oleksii Danylyuk <danilyuk@bigmir.net>
2020-02-25 12:18:50 +0200

    add os

--- devops/monitoring/mn.py
+++ devops/monitoring/mn.py
@@ -1,32 +1,16 @@
+import os
+import platform
 import psutil as p
 import sys
-
-print(len(sys.argv))
-
-
-
-if sys.argv[1] == "cpu":
-    print("cpu rules")
-elif sys.argv[1] == "mem":
-    print("mem rules")
-else:
-    print("put correct arguments")
-
-"""cpu"""
-print('cpu use:',p.cpu_percent(interval=1))
-"""memory"""
-memusage = p.virtual_memory()
-print('memory use:', memusage)
-"""disk partitions"""
-disk_part = p.disk_partitions()
-print('disk part:',disk_part)
-"""disk usage"""
-disk_usage = p.disk_usage("/")
-print('disk use:',disk_usage)
-
-"""boot time"""
 import datetime as dt
+
+argumentList = sys.argv
+print (argumentList)
+"""boot time"""
 boot = p.boot_time()
 #print(boot)
 x = dt.datetime.fromtimestamp(boot).strftime("%Y-%m-%d %H:%M:%S")
 print(x)
+"""os"""
+platform.system()
+platform.release()
