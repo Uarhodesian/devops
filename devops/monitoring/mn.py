c2b81d6baa8355cd8fd8b69260d55c765861e5b0
Oleksii Danylyuk <danilyuk@bigmir.net>
2020-02-25 12:51:22 +0200

    ready to realese

--- devops/monitoring/mn.py
+++ devops/monitoring/mn.py
@@ -5,19 +5,26 @@ import sys
 import datetime as dt
 
 argumentList = sys.argv
+argumentList += argumentList
 print (argumentList)
 """boot time"""
 boot = p.boot_time()
-#print(boot)
 x = dt.datetime.fromtimestamp(boot).strftime("%Y-%m-%d %H:%M:%S")
 print(x)
 """os"""
 print(platform.system())
 print(platform.release())
-#print("use arguments '"'mem'"' or '"'cpu'"' to to specify which metrics you want to print")
 if argumentList[1] == "mem":
-    print("mem rules")
+    print("======")
+    print("mem")
+    print("======")
+    print('mem:', p.virtual_memory())
 elif argumentList[1] == "cpu":
-    print("cpu rules")
+    print("======")
+    print("cpu")
+    print("======")
+    print('per-cpu:', p.cpu_percent(interval=1))
+    print('per-cpu:', p.cpu_stats())
+    print('per-cpu:', p.cpu_freq())
 else:
-    print("Wrong arguments! Use arguments '"'mem'"' or '"'cpu'"' to to specify which metrics you want to print")
+    print("Wrong arguments!\nUse arguments '"'mem'"' or '"'cpu'"' to to specify which metrics do you want to print!")
