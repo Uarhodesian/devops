db9b89a8a61ce601a2a7946c27bcb3516d08c4bc
Oleksii Danylyuk <danilyuk@bigmir.net>
2020-02-25 09:58:43 +0200

    add platform

--- devops/monitoring/metrics .py
+++ devops/monitoring/metrics .py
@@ -1,4 +1,10 @@
+import os
+import platform
 import psutil as p
+"""os"""
+print(platform.system())
+print(platform.release())
+print(os.name)
 """cpu"""
 print('cpu use:',p.cpu_percent(interval=1))
 """memory"""
@@ -10,12 +16,8 @@ print('disk part:',disk_part)
 """disk usage"""
 disk_usage = p.disk_usage("/")
 print('disk use:',disk_usage)
-#"""network connection"""
-#net = p.net_io_counters()
-#print(net)
 """boot time"""
 import datetime as dt
 boot = p.boot_time()
-#print(boot)
 x = dt.datetime.fromtimestamp(boot).strftime("%Y-%m-%d %H:%M:%S")
 print(x)
